96fc50709541ddc04d21a9eb1036d4fe52844e8a
Henry <hes899@mail.missouri.edu>
2019-06-14 15:19:54 -0500

    Initial Commit

---
+++ Swift Basic/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basic
+//
+//  Created by Henry Sills on 6/14/19.
+//  Copyright © 2019 Henry Sills. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
